23eac97846555f00bec745cba66afd1e923e58de
kjs <kjs26662@gmail.com>
2024-12-02 15:29:38 +0900

    first commit

---
+++ src/main/java/com/example/aplace/APlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplace/APlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
