0dc2b35db992ea0117ce9e5efb699892ad0ce4c5
kjs <kjs26662@gmail.com>
2024-12-02 18:03:33 +0900

    first commit

--- src/main/java/com/example/aplace/controller/HomeController.java
+++ src/main/java/com/example/aplace/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.example.aplace.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
-        return "Welcome to A-Place";
+        return "home";
     }
 }
