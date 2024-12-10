f6d203d171e6a04be6c5b7aac9f48715408bd92c
kjs <kjs26662@gmail.com>
2024-12-10 12:42:11 +0900

    ApllicationProperties gitignore

--- src/main/java/com/example/aplace/APlaceApplication.java
+++ src/main/java/com/example/aplace/APlaceApplication.java
@@ -1,13 +1,24 @@
 package com.example.aplace;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class APlaceApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(APlaceApplication.class, args);
 	}
-
 }
